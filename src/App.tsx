4bee77829b10076137f6afebcbb092613bc9a642
vakaer <w.mujeeb@algosoftco.com>
2025-05-11 17:15:17 +0500

    remove css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import './App.css';
 import { ModelViewer } from './components/ModelViewer';
 import hotspots from './data/hotspots';
 
 function App() {
   return (
-    <div className="App">
       <ModelViewer annotations={hotspots} />
-    </div>
   );
 }
 
